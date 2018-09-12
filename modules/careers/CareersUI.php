8958f92cb82ce125ad785c37ed7a76ac08b0c709
TomNagle <tomanagle@gmail.com>
2018-09-12 16:05:11 +0100

    Add label to file

--- modules/careers/CareersUI.php
+++ modules/careers/CareersUI.php
@@ -564,7 +564,7 @@ Array.prototype.forEach.call( inputs, function( input )
 	{
 		var fileName = "";
 		if( this.files && this.files.length > 1 )
-			fileName = ( this.getAttribute("data-multiple-caption" || "" ).replace("{count}", this.files.length );
+			fileName = ( this.getAttribute("data-multiple-caption" || "" ).replace("{count}", this.files.length )
 		else
 			fileName = e.target.value.split( "\\").pop();
 
